9763ff107ca172c4240d4783fd1537679afc5180
root <aobersnel@apnex.com.au>
2021-06-01 21:44:47 +1000

    Initial..

---
+++ main.tf
@@ -0,0 +1,29 @@
+terraform {
+	required_providers {
+		vsphere = "~> 1.26.0"
+	}
+}
+provider "vsphere" {
+	vsphere_server		= "vcenter.lab01.one"
+	user			= "administrator@vsphere.local"
+	password		= "VMware1!SDDC"
+	allow_unverified_ssl	= true
+}
+
+module "avi-controller" {
+	source		= "./terraform-avi-controller"
+
+	### vsphere variables
+	datacenter	= "core"
+	cluster		= "core"
+	datastore	= "ds-esx11"
+	host		= "esx11.lab01.one"
+	network		= "vss-vmnet"
+
+	### appliance variables
+	vm_name		= "avic.lab01.one"
+	remote_ovf_url	= "http://172.16.10.1:9000/iso/controller-20.1.5-9148.ova"
+	mgmt-ip		= "172.16.10.119"
+	mgmt-mask	= "255.255.255.0"
+	default-gw	= "172.16.10.1"
+}
